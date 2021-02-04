d25dfb7412a20737d78a35642589d42b3eaa829e
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-02-04 13:58:13 +0100

    Add button and header to storybook

---
+++ src/components/Headercomponent.stories.js
@@ -0,0 +1,8 @@
+import Headercomponent from './Headercomponent'
+
+export default {
+  title: 'Headercomponent',
+  component: Headercomponent,
+}
+
+export const base = () => Headercomponent()
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import Headercomponent from './components/Headercomponent'
+import Button from './components/Button'
+
+const headercomponent = Headercomponent()
+
+document.body.append(headercomponent)
+
+const button = Button('Click me!')
+
+document.body.append(button)
