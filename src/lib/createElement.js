0347f09d2f5f13fe0637587d5f0a63dcd4dd8302
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-02-09 22:54:24 +0100

    Add createpage

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -2,6 +2,6 @@ export default function createElement(type, props, ...children) {
   // Sachen gesamelt
   const el = document.createElement(type)
   Object.assign(el, props)
-  children && el.append(...children) // Sachen verteilt
+  children && el.append(...children.map(child => child.el ?? child)) // Sachen verteilt
   return el
 }
