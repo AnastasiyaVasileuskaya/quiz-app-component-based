1a1d0d83d62e0f7861d3ec824ac4f68b20d3bbf4
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-02-09 20:27:51 +0100

    Change index.js

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -1,8 +1,9 @@
 import createElement from '../../lib/createElement'
+import Button from '../Button'
 import './CreateForm.css'
 
-export default function CreateForm() {
-  const el = createElement('form', {
+export default function CreateForm(onSubmit) {
+  const form = createElement('form', {
     className: 'CreateForm',
     innerHTML: `
     <label>Question:
@@ -11,7 +12,15 @@ export default function CreateForm() {
     <label>Answer:
     <input name="answer" placeholder="Your answer">
     </label>
-    <button>Create Question</button>`,
+    `,
   })
-  return el
+  form.append(Button('Create!'))
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    const { question: questionInput, answer: answerInput } = form.elements
+    const question = questionInput.value
+    const answer = answerInput.value
+    onSubmit(question, answer)
+  })
+  return form
 }
